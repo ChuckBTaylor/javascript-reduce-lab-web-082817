897760caf27b20a77742ed8521833cd856afe4dc
ChuckBTaylor <WLScripps@gmail.com>
2017-10-09 18:31:32 -0400

    Done.

--- index.js
+++ index.js
@@ -1,18 +1,38 @@
 const batteryBatches = [4, 5, 3, 4, 4, 6, 5];
 
 const monologueLines = [
-  'Who are you talking to right now?',
-  'Who is it you think you see?',
-  'Do you know how much I make a year?',
-  'I mean, even if I told you, you wouldn\'t believe it.',
-  'Do you know what would happen if I suddenly decided to stop going into work?',
-  'A business big enough that it could be listed on the NASDAQ goes belly up.',
-  'Disappears!',
-  'It ceases to exist without me.',
-  'No, you clearly don\'t know who you\'re talking to, so let me clue you in.',
-  'I am not in danger, Skyler.',
-  'I am the danger.',
-  'A guy opens his door and gets shot and you think that of me?',
-  'No.',
-  'I am the one who knocks!'
+  "Who are you talking to right now?",
+  "Who is it you think you see?",
+  "Do you know how much I make a year?",
+  "I mean, even if I told you, you wouldn't believe it.",
+  "Do you know what would happen if I suddenly decided to stop going into work?",
+  "A business big enough that it could be listed on the NASDAQ goes belly up.",
+  "Disappears!",
+  "It ceases to exist without me.",
+  "No, you clearly don't know who you're talking to, so let me clue you in.",
+  "I am not in danger, Skyler.",
+  "I am the danger.",
+  "A guy opens his door and gets shot and you think that of me?",
+  "No.",
+  "I am the one who knocks!"
 ];
+
+const totalBatteries = batteryBatches.reduce(function(battery, agg) {
+  return (agg += battery);
+}, 0);
+
+const default_value = {
+  get: function(target, name) {
+    return target.hasOwnProperty(name) ? target[name] : 0;
+  }
+};
+
+const wordCountArray = monologueLines.map(function(line) {
+  return line.split(" ").length;
+});
+
+const wordCountMap = new Proxy({}, default_value);
+
+wordCountArray.forEach(function(length) {
+  wordCountMap[length] += 1;
+});
